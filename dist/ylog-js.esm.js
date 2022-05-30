a71675911c020bcbd83aa0495a87df9abb764bdf
cqqyy321 <seayman@163.com>
2022-05-30 13:14:44 +0800

    add: array and date utils

--- dist/ylog-js.esm.js
+++ dist/ylog-js.esm.js
@@ -6,4 +6,50 @@ function sumArr(arr, i) {
     return arr[i] + sumArr(arr, i + 1);
 }
 
-export { sumArr };
+function parseTime(timestamp, cFormat) {
+    if (cFormat === void 0) { cFormat = '{y}-{m}-{d} {h}:{i}'; }
+    if (arguments.length === 0 || !timestamp) {
+        return '';
+    }
+    var date;
+    if (typeof timestamp === 'object') {
+        date = timestamp;
+    }
+    else {
+        if ((typeof timestamp === 'string')) {
+            if ((/^[0-9]+$/.test(timestamp))) {
+                // support "1548221490638"
+                timestamp = parseInt(timestamp);
+            }
+            else {
+                // support safari
+                // https://stackoverflow.com/questions/4310953/invalid-date-in-safari
+                timestamp = timestamp.replace(new RegExp(/-/gm), '/');
+            }
+        }
+        if ((typeof timestamp === 'number') && (timestamp.toString().length === 10)) {
+            timestamp = timestamp * 1000;
+        }
+        date = new Date(timestamp);
+    }
+    var formatObj = {
+        y: date.getFullYear(),
+        m: date.getMonth() + 1,
+        d: date.getDate(),
+        h: date.getHours(),
+        i: date.getMinutes(),
+        s: date.getSeconds(),
+        a: date.getDay()
+    };
+    return cFormat.replace(/{([ymdhisa])+}/g, function (result, key) {
+        // @ts-ignore
+        var value = formatObj[key];
+        // Note: getDay() returns 0 on Sunday
+        if (key === 'a') {
+            return ['日', '一', '二', '三', '四', '五', '六'][value];
+        }
+        return value.toString().padStart(2, '0');
+    });
+}
+
+export { parseTime, sumArr };
